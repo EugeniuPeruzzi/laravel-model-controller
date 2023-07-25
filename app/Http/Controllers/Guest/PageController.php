8e355ec701a080818b49f46057276ebe7659eb41
EugeniuPeruzzi <jaka1997@live.it>
2023-07-25 15:53:47 +0200

    conected model 'Comics' to page controller

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Guest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+// utilizzo il model del data base con il nome Comics
+use App\Models\Comics;
+
+
 class PageController extends Controller
 {
     public function index(){
